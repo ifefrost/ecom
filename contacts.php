1d346fe63b7b0f700fad4a30a80557ad3058add9
Savankumar patel <113946649+Savan708@users.noreply.github.com>
2022-12-13 21:31:46 -0500

    contact us

--- contacts.php
+++ contacts.php
@@ -1,5 +1,5 @@
 <?php
-
+require("submit.php");
 require_once("functions.inc.php");
 redirectIfNotLoggedIn();
 
@@ -7,6 +7,8 @@ redirectIfNotLoggedIn();
 <!doctype html>
 <html>
 <?php pageheader('About') ?>
+
+
         <style>
 .column {
   float: left;
@@ -26,29 +28,81 @@ redirectIfNotLoggedIn();
   background-color: #474e5d;
   color: white;
 }
-
-
-
-
-
 .title {
   color: grey;
 }
 
-.button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
+.contact {
+    background-color: #fff;
+    width: 500px;
+    margin: 0 auto;
+    box-shadow: 0px 0px 5px 0px rgba(0,0,0,.2);
 }
+.contact .fields {
+    position: relative;
+    padding: 15px;
+}
+.contact input[type="text"], .contact input[type="email"] {
+    display: block;
+    margin-top: 15px;
+    padding: 15px;
+    border: 1px solid #dfe0e0;
+    width: 100%;
+}
+.contact input[type="text"]:focus, .contact input[type="email"]:focus {
+    border: 1px solid #c6c7c7;
+}
+.contact input[type="text"]::placeholder, 
+.contact input[type="email"]::placeholder, 
+.contact textarea::placeholder {
+    color: #858688;
+}
+.contact textarea {
+    resize: none;
+    margin-top: 15px;
+    padding: 15px;
+    border: 1px solid #dfe0e0;
+    width: 100%;
+    height: 150px;
+}
+.contact textarea:focus {
+    border: 1px solid #c6c7c7;
+}
+.contact input[type="submit"] {
+    display: block;
+    margin-top: 15px;
+    padding: 15px;
+    border: 0;
+    background-color: #518acb;
+    font-weight: bold;
+    color: #fff;
+    cursor: pointer;
+    width: 100%;
+}
+.contact input[type="submit"]:hover {
+    background-color: #3670b3;
+}
+.contact input[name="email"] {
+    position: relative;
+    display: block;
+}
+.contact label {
+    position: relative;
 
-.button:hover {
-  background-color: #555;
+}
+.contact label i {
+    position: absolute;
+    color: #dfe2e5;
+    top: 31px;
+    left: 15px;
+    z-index: 10;
+}
+.contact label i ~ input {
+    padding-left: 45px !important;
+}
+.contact .responses {
+    padding: 15px;
+    margin: 0;
 }
 p{
     font-size:10px;
@@ -62,64 +116,38 @@ p{
             </style>
         <body class="container">
         <div class="contact-section">
-  <h1>Contact Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
+  
+  <div class="form-container">
+  <div class="col-50"  >
+  <!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,minimum-scale=1">
+		<title>Contact Form</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<form class="contact" method="post" action="submit.php">
+			<h1>Contact Form</h1>
+			<div class="fields">
+				<label for="email">
+					<i class="fas fa-envelope"></i>
+					<input id="email" type="email" name="email" placeholder="Your Email" required>
+				</label>
+				<label for="name">
+					<i class="fas fa-user"></i>
+					<input type="text" name="name" placeholder="Your Name" required>
+				<label>
+				<input type="text" name="subject" placeholder="Subject" required>
+				<textarea name="msg" placeholder="Message" required></textarea>
+			</div>
+			<input type="submit">
+		</form>
+	</body>
+</html>
 </div>
-
-<h2 style="text-align:center">Our Team</h2>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="./assets/img/team.webp" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Dhairya</h2>
-        <p class="title">CEO & Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>jane@example.com</p>
-  
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="./assets/img/team.webp" alt="Mike" style="width:100%">
-      <div class="container">
-        <h2>Bini</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>mike@example.com</p>
-      
-      </div>
-    </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <img src="./assets/img/team.webp" alt="John" style="width:100%">
-      <div class="container">
-        <h2>Iffe</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
-      
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="./assets/img/team.webp" alt="John" style="width:100%">
-      <div class="container">
-        <h2>Savan</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
-      
-      </div>
-    </div>
-  </div>
 </div>
 
 
